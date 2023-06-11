eb7178d4cb0cb9e9964965dd90f9afa5908012b6
felixburn <113917256+felixburn@users.noreply.github.com>
2023-06-12 01:39:25 +0800

    maven项目搭建+导入前端页面

---
+++ reggie_take_out/src/main/java/com/example/reggie_take_out/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.example.reggie_take_out.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("添加静态资源映射");
+        registry.addResourceHandler("/backend/**")
+                .addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**")
+                .addResourceLocations("classpath:/front/");
+    }
+}
