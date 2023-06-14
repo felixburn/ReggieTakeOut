c648b7cf88ab6dfca3822f00f2c4e6b412acfd23
felixburn <113917256+felixburn@users.noreply.github.com>
2023-06-14 22:06:17 +0800

    修改分类

--- reggie_take_out/src/main/java/com/example/reggie_take_out/controller/CategoryController.java
+++ reggie_take_out/src/main/java/com/example/reggie_take_out/controller/CategoryController.java
@@ -64,4 +64,17 @@ public class CategoryController {
         categoryService.remove(ids);
         return R.success("分类信息删除成功");
     }
+
+    /**
+     * 根据id来修改分类信息
+     * @param category
+     * @return
+     */
+    // 	http://localhost:8080/category
+    @PutMapping()
+    public R<String> update(@RequestBody Category category){
+        log.info("修改分类信息：{}",category);
+        categoryService.updateById(category);
+        return R.success("修改分类信息成功");
+    }
 }
