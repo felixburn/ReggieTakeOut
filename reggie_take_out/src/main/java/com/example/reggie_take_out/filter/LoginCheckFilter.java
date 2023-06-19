0cadcd61a7ea1272dc24be783aa92bb30e16321b
felixburn <113917256+felixburn@users.noreply.github.com>
2023-06-20 06:39:29 +0800

    手机验证码登录

--- reggie_take_out/src/main/java/com/example/reggie_take_out/filter/LoginCheckFilter.java
+++ reggie_take_out/src/main/java/com/example/reggie_take_out/filter/LoginCheckFilter.java
@@ -38,7 +38,10 @@ public class LoginCheckFilter implements Filter {
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
-                "/front/**"
+                "/front/**",
+                "/common/**",
+                "/user/sendMsg", //移动端发送短信
+                "/user/login" //移动端登录
         };
         // 2.判断本次请求是否需要处理
         boolean check = check(urls, requestURI);
@@ -48,7 +51,7 @@ public class LoginCheckFilter implements Filter {
             filterChain.doFilter(request,response);
             return;
         }
-        // 4.判断登录状态，如果已登录，直接放行
+        // 4.1判断登录状态，如果已登录，直接放行
         //   开发中最好校验session是否合法
         if(request.getSession().getAttribute("employee") != null){
             log.info("用户已登录，用户id为{}",request.getSession().getAttribute("employee"));
@@ -56,6 +59,14 @@ public class LoginCheckFilter implements Filter {
             Long empId = (Long)request.getSession().getAttribute("employee");
             BaseContext.setCurrentId(empId);
             filterChain.doFilter(request,response);
+            return;
+        }
+        //  4-2、判断登录状态，如果已登录，则直接放行
+        if (request.getSession().getAttribute("user") != null) {
+            log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("user"));
+            Long userId= (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
+            filterChain.doFilter(request, response);
             return;
         }
         // 5.如果未登录，则返回未登录结果，通过输出流方式向客户端页面响应数据
